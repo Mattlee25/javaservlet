36ca1916513b6259812fca765e002a04bf56e222
Mattlee25 <36556825+Mattlee25@users.noreply.github.com>
2021-03-23 18:57:18 -0400

    Update attributeServlet.java

--- swe432tomcat/src/main/java/servlet/attributeServlet.java
+++ swe432tomcat/src/main/java/servlet/attributeServlet.java
@@ -78,8 +78,8 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    while (e.hasMoreElements())
    {
       String att_name  = (String) e.nextElement();
-      String att_value = (String) session.getAttribute(att_name);
-      String att_car   = (String) session.getAttribute(att_value);
+      String att_value = (String) e.nextElement();
+      String att_car   = (String) session.getAttribute(att_name);
 
       out.print  ("<br><b>Name:</b> ");
       out.println(att_name);
