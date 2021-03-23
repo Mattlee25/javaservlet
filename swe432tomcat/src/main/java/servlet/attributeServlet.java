dfb1e8ae5a1d17c1c25c07e9215c9b11d1bbbbb3
Mattlee25 <36556825+Mattlee25@users.noreply.github.com>
2021-03-23 19:30:09 -0400

    Update attributeServlet.java

--- swe432tomcat/src/main/java/servlet/attributeServlet.java
+++ swe432tomcat/src/main/java/servlet/attributeServlet.java
@@ -36,6 +36,7 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
       if ((name != null && name.length() > 0) && (value != null && value.length() > 0) && (car != null && car.length() > 0))
       {
          session.setAttribute(name, value);
+         session.setAttribute(name, car);
       }
 
    }
