964c8d3a9fda11840faa4affe0f079f55bfc9b41
Mattlee25 <36556825+Mattlee25@users.noreply.github.com>
2021-04-01 16:23:16 -0400

    Update ServletPersistentPost.java

--- swe432tomcat/src/main/java/servlet/ServletPersistentPost.java
+++ swe432tomcat/src/main/java/servlet/ServletPersistentPost.java
@@ -21,14 +21,16 @@ public class ServletPersistentPost extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
     PrintWriter out = response.getWriter();
     response.setContentType("text/html");
+    String name = request.getParameter("submit");
+    if (name == "") {
         out.println("<body style=\"text-align:center;\">");
-        out.println("<h1>Homework 7</h1>");
+        out.println("<h1>Homework 6</h1>");
         out.println("<h3>Group members: Raul Puza, Antonio Stockel, Matthew Lee</h3>");
         out.println("<div style=\"display:inline-block;width:400px;background-color:rgb(187,187,187);border-radius:5px;padding:10px0;margin:50px0;\">");
         out.println("<form method=\"post\" style=\"margin:20px0;width:50%;display:inline-block;\">");// action=\"https://cs.gmu.edu:8443/offutt/servlet/formHandler\">");
         out.println("<h4>Input multiple strings with a space in between</h4>");
         out.println("<textarea name=\"array\" id=\"array\" value=\"\" style=\"margin-top:10px;border:none;width:100%;height:100px;\"></textarea><br>");
-        out.println("<button name=\"submit\" value=\"\" type=\"submit\" onClick=\"display()\" style=\"float:left;\">submit</button>");
+        out.println("<button name=\"submit\" value=\"submission\" type=\"submit\" onClick=\"display()\" style=\"float:left;\">submit</button>");
         out.println("<button name=\"submit\" value=\"entries\"type=\"submit\" onClick=\"display()\" style=\"float:right;\">entries</button>");
         out.println("<button name=\"submit\" value=\"clear\"type=\"submit\" onClick=\"display()\" style=\"float:right;\">clear file</button>");
         out.println("</form>");
@@ -60,9 +62,12 @@ public class ServletPersistentPost extends HttpServlet {
             out.println("<input name=\"reverse-sorted-array\" id=\"reverse-sorted-array\" type=\"text\" value=\"\">");
             out.println("</div>");
             out.println("</body>");
-    ServletContext servletContext = getServletContext();
+    } else {
+            ServletContext servletContext = getServletContext();
     RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("/HelloPersistent");
     requestDispatcher.forward(request, response);
+    }
+    
   }
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
     doGet(request, response);
